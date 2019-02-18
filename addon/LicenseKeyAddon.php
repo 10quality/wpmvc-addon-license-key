9ecba892b790690516a72c64f0ba34d326cb57b2
Alejandro Mostajo <amostajo@gmail.com>
2019-02-17 23:20:13 -0600

    Bug fixes.
    Bug fixes.

--- addon/LicenseKeyAddon.php
+++ addon/LicenseKeyAddon.php
@@ -13,7 +13,7 @@ use WPMVC\Addons\LicenseKey\Utility\Encryption;
  * @author Cami Mostajo
  * @package WPMVC\Addons\LicenseKey
  * @license MIT
- * @version 1.1.4
+ * @version 1.1.5
  */
 class LicenseKeyAddon extends Addon
 {
@@ -170,7 +170,11 @@ class LicenseKeyAddon extends Addon
                 'main'          => $this->main,
                 'license_key'   => $this->get_license_key(),
             ];
-            if ( $params['license_key'] && $params['license_key'] !== false && $params['license_key']->data->expire ) {
+            if ( $params['license_key']
+                && $params['license_key'] !== false
+                && isset( $params['license_key']->data )
+                && $params['license_key']->data->expire
+            ) {
                 // Check extension
                 if ( $params['license_key'] === false || !isset( $params['license_key']->data->ctoken ) || $params['license_key']->data->ctoken === null )
                     return;
